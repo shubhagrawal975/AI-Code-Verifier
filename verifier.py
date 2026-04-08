1a2687fc9fb0695966a865e1f4053a06055deb38
Shubh Agrawal <infoshubhagrawal@gmail.com>
2026-04-08 21:53:36 +0530

    Implement code verification function
    Added a function to verify user code against reference code using generated test inputs.

--- verifier.py
+++ verifier.py
@@ -1,1 +1,23 @@
 from utils import run_code, generate_test_inputs
+
+def verify_code(user_code, reference_code):
+  test_inputs=generate_test_inputs()
+  passed=0
+  print("\n==========Running Test Cases==========\n")
+  for i,test_input in enumerate(test_inputs):
+    user_output=run_code(user_code,test_input)
+    ref_output=run_code(reference_code,test_input)
+    print(f"Test Case : {i+1}")
+    print(f"Input: {test_input}")
+    
+    if (user_output==ref_output):
+      print("Result: Pass\n")
+      passed+=1
+    else:
+      print("Result: Fail\n")
+      print("Expected Output: ", ref_output)
+      print("User Output: ", user_output)
+
+  print("==========FINAL RESULT==========")
+  print(f"passed {passed}/{len(test_inputs)} test cases")
+    
